41617ad69d1dbfd902eac546a3ca1325bcff0fa0
db104 <db104@hdm-stuttgart.de>
2017-01-17 14:18:47 +0100

    Add files via upload

---
+++ login.php
@@ -0,0 +1,60 @@
+<?php
+session_start();
+$dsn="mysql:host=localhost;dbname=u-db104"; #Datenbankverbindung aufbauen
+$dbuser="db104";    #db username
+$dbpass="anohk4Aepu";   #db passwort
+try {
+    $db=new PDO($dsn,$dbuser,$dbpass); #kreiren der variable $db mit Daten für Datenbankverbindung
+}
+catch(PDOException $e){
+    echo $e->getMessage();
+    die();
+}
+?>
+<?php
+
+if(isset($_GET['login'])) {     #loginformular senden
+    $username = $_POST['username']; #eingegebenen Username $username zuordenen
+    $password = $_POST['password']; #eingegebenes Passwort $password zuordnen
+
+    $statement = $db->prepare("SELECT * FROM person WHERE username = :username"); #mit der Variable $statement alle usernames in der Datenbank 'person' vorbereiten
+    $result = $statement->execute(array('username' => $username)); #eingegebenen username mit username aus Datenbank abgleichen
+    $user = $statement->fetch(); #variable username erstellen mit dem entsprechenden uername aus $statement
+
+    //Überprüfung des Passworts
+    if ($user !== false && password_verify($password, $user['password'])) { #wenn $user nicht falsch ist und das Passwort aus einem hash gelesen werden kann
+        $_SESSION['userid'] = $user['id']; #session id erzeugen mit der bezeichung 'userid'
+        die('Login erfolgreich. Falls sie nicht automatisch weitergeleitet werden klicken sie bitte hier: <a href="geheim.php">internen Bereich</a>'); #erfolgreich, weiterleiten auf internen Bereich
+?>
+         <meta http-equiv="refresh" content="5; URL=geheim.php">
+<?php
+    } else {
+        $errorMessage = "Username oder Passwort wurde falsch eingegeben";  #login klappt nicht
+    }
+
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+
+<?php
+if(isset($errorMessage)) {
+    echo $errorMessage;
+}
+?>
+
+<form action="?login=1" method="post">
+    Username:<br>
+    <input type="text" size="40" maxlength="250" name="username"><br><br>
+
+    Dein Passwort:<br>
+    <input type="password" size="40"  maxlength="250" name="password"><br>
+
+    <input type="submit" value="Abschicken">
+</form>
+</body>
+</html>
