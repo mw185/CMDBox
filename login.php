2c0824a3bb106aad99b461be7fe078de06c98edf
db104 <db104@hdm-stuttgart.de>
2017-02-08 17:44:23 +0100

    Heil PHP

--- login.php
+++ login.php
@@ -2,19 +2,13 @@
 session_start();
 
 include "connection.php";
-if(isset($errorMessage)) {
-    echo $errorMessage;
-}
-
-
 include "loginpage.html";
-if(isset($errorMessage)) {
-echo $errorMessage;
-}
+
 
 if(isset($_GET['login'])) {     #loginformular senden
     $username = $_POST['username']; #eingegebenen Username $username zuordenen
     $password = $_POST['password']; #eingegebenes Passwort $password zuordnen
+
     $statement = $db->prepare ("SELECT * FROM person WHERE username = :username"); #mit der Variable $statement alle usernames in der Datenbank 'person' vorbereiten
     $result = $statement->execute(array('username' => $username)); #eingegebenen username mit username aus Datenbank abgleichen
     $user = $statement->fetch(); #variable username erstellen mit dem entsprechenden uername aus $statement
@@ -23,20 +17,9 @@ if(isset($_GET['login'])) {     #loginformular senden
     if ($user !== false && password_verify($password, $user['password'])) { #wenn $user nicht falsch ist und das Passwort aus einem hash gelesen werden kann
         $_SESSION['userid'] = $user['id']; #session id erzeugen mit der bezeichung 'userid'
 
-        die
-?>
-
-
-        Login erfolgreich. Falls sie nicht automatisch weitergeleitet werden klicken sie bitte hier:
-
-<a href="http://www.bild.de">internen Bereich</a>'); #erfolgreich, weiterleiten auf internen Bereich
-
-        <meta http-equiv="refresh" content="5; URL = showuploads.php">
-        
-        <?php
-
-    } else {
-        $errorMessage = "Username oder Passwort wurde falsch eingegeben";  #login klappt nicht
-    }
+            die('Login erfolgreich. Weiter zu <a href="showuploads.php">internen Bereich</a>');
+        } else {
+            $errorMessage = "E-Mail oder Passwort war ungültig<br>";
+        }
 }
-?>
+    ?>L
