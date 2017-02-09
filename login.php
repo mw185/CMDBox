bdc9a89059c7f44366956226c30a4fcfe4fae324
marliswiedemann <mw185@hdm-stuttgart.de>
2017-02-09 15:08:33 +0100

    connection

--- login.php
+++ login.php
@@ -1,15 +1,21 @@
 <?php
 session_start();
 
-include "connection.php";
+include 'connection.php';
+if(isset($errorMessage)) {
+    echo $errorMessage;
+}
+
+
 include "loginpage.html";
 
+$showFormular = true; #Registrierungsformular wird angezeigt
 
 if(isset($_GET['login'])) {     #loginformular senden
     $username = $_POST['username']; #eingegebenen Username $username zuordenen
     $password = $_POST['password']; #eingegebenes Passwort $password zuordnen
 
-    $statement = $db->prepare ("SELECT * FROM person WHERE username = :username"); #mit der Variable $statement alle usernames in der Datenbank 'person' vorbereiten
+    $statement = $db->prepare("SELECT * FROM person WHERE username = :username"); #mit der Variable $statement alle usernames in der Datenbank 'person' vorbereiten
     $result = $statement->execute(array('username' => $username)); #eingegebenen username mit username aus Datenbank abgleichen
     $user = $statement->fetch(); #variable username erstellen mit dem entsprechenden uername aus $statement
 
@@ -18,11 +24,17 @@ if(isset($_GET['login'])) {     #loginformular senden
         $_SESSION['userid'] = $user['id']; #session id erzeugen mit der bezeichung 'userid'
         $_SESSION['loggedin'] = 1;
 
+        if ($result) {
+            $showFormular = false;  #Formular wird nicht mehr angezeigt
+            # Bestätigungsmail senden
 
 
-            die('Login erfolgreich. Weiter zu <a href="showuploads.php">internen Bereich</a>');
+            die('Login erfolgreich. Weiter zu <a href="uploadseite.html">internen Bereich</a>');
         } else {
             $errorMessage = "E-Mail oder Passwort war ungültig<br>";
         }
+    }
 }
-    ?>
+
+
+?>
