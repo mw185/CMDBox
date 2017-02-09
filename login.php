5651d49af36fec266393378d91d9fe5abe32e32c
db104 <db104@hdm-stuttgart.de>
2017-02-09 15:29:30 +0100

    Heil PHP

--- login.php
+++ login.php
@@ -23,6 +23,7 @@ if(isset($_GET['login'])) {     #loginformular senden
     if ($user !== false && password_verify($password, $user['password'])) { #wenn $user nicht falsch ist und das Passwort aus einem hash gelesen werden kann
         $_SESSION['userid'] = $user['id']; #session id erzeugen mit der bezeichung 'userid'
         $_SESSION['loggedin'] = 1;
+        header("Location: uploadseite.html");
 
         if ($result) {
             $showFormular = false;  #Formular wird nicht mehr angezeigt
