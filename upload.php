36329ca91d42b0a9c7feb2017657ce1a9e42cdb9
db104 <db104@hdm-stuttgart.de>
2017-02-09 17:27:20 +0100

    Heil PHP

--- upload.php
+++ upload.php
@@ -9,7 +9,7 @@ $directorywert = md5($_SESSION['username']);
 
 
 // Dateien werden in den jeweiligen Ordner basierend auf dem Email Hash abgelegt
-$target_dir = "file/$directorywert/";
+$target_dir = "Uploads/$directorywert/";
 
 
 // Mithilfe von preg_replace werden ungültige Zeichen, die zu Problemen führen künnen, ersetzt.
