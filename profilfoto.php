f92557b0a20a0d0d18129b99dd33ad7b87acb60a
marliswiedemann <mw185@hdm-stuttgart.de>
2017-02-11 14:53:11 +0100

    connection

--- profilfoto.php
+++ profilfoto.php
@@ -1,25 +1,25 @@
 <?php
 // Session starten
 session_start();
+if (isset($_GET ['profilfoto'])) {
+    $dir = "Profilbild/";
+    $newname = $_SESSION ['username'];
+    if (isset($_FILES['file'])) {
+        $zielname = basename($_FILES["file"]["name"]);
+        if (move_uploaded_file($newname, $dir . $zielname)) {
+            rename($dir . $zielname, $dir . $newname . '.jpeg');
+            header('location: profil.php');
+        } else {
+            echo "Fehler";
+        }
 
-$dir = "Profilbild/";
-$newname = $_SESSION ['username'];
-if (isset($_FILES['file'])) {
-    $zielname = basename ($_FILES["file"]["name"]);
-    if (move_uploaded_file($newname, $dir.$zielname)){
-    rename($dir.$zielname, $dir.$newname.'.jpeg');
-    header ('location: profil.php');
     }
-    else {
-        echo "Fehler";
-    }
-
 }
 ?>
 
-<form method="POST" enctype="multipart/form-data" action="profilfoto.php">
+<form method="POST" enctype="multipart/form-data" action="profilfoto.php?profilfoto=1">
     <p><input type="file" name="file" size="20">
-        <input type="submit" value="hochladen" name="Bild hochladen"></p>
+        <input type="submit" value="Bild hochladen" name="bild_hochladen"></p>
 </form>
 
 
