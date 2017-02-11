0728554801c0cf8c84606854a0b5c8bf02c7d586
marliswiedemann <mw185@hdm-stuttgart.de>
2017-02-11 14:46:05 +0100

    connection

--- profilfoto.php
+++ profilfoto.php
@@ -3,17 +3,24 @@
 session_start();
 
 $dir = "Profilbild/";
-$newname = $_SESSION ['dir'];
+$newname = $_SESSION ['username'];
 if (isset($_FILES['file'])) {
     $zielname = basename ($_FILES["file"]["name"]);
     if (move_uploaded_file($newname, $dir.$zielname)){
-    rename($dir.$zielname, $dir.$newname);
-    header ('location: showuploads.php');
+    rename($dir.$zielname, $dir.$newname.'.jpeg');
+    header ('location: profil.php');
     }
-    else{
+    else {
         echo "Fehler";
     }
 
 }
-
 ?>
+
+<form method="POST" enctype="multipart/form-data" action="profilfoto.php">
+    <p><input type="file" name="file" size="20">
+        <input type="submit" value="hochladen" name="Bild hochladen"></p>
+</form>
+
+
+
