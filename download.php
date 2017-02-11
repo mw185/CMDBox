00ca622c27563280b005e9a2cdce90c5bb05538c
db104 <db104@hdm-stuttgart.de>
2017-02-11 19:27:02 +0100

    Heil PHP

--- download.php
+++ download.php
@@ -3,6 +3,13 @@
 if (isset($_GET['file']) && basename($_GET['file']) == $_GET['file']) {
     $filename = $_GET['file'];
 }
+else {
+    $filename = NULL;
+}
+
+if(!$filename) {
+
+}
 
 $path = "Uploads/" . $filename;
 $mime = mime_content_type($path);
@@ -11,20 +18,23 @@ $fsize = filesize ($path);
 echo $path;
 var_dump($path);
 
+else {
 
-if (file_exists($path) && is_readable ($path)) {
-    header ('Content-Type: '.$mime);
-    header ('Content-Length: '.$fsize);
-    header ('Content-Disposition: attachment; filename='.$filename);
-    header ('Content-Transfer-Encoding: binary');
 
-    $file = @ fopen ($path, 'rb');
+    if (file_exists($path) && is_readable($path)) {
+        header('Content-Type: ' . $mime);
+        header('Content-Length: ' . $fsize);
+        header('Content-Disposition: attachment; filename=' . $filename);
+        header('Content-Transfer-Encoding: binary');
 
-    if ($file) {
-        fpassthru($file);
-        exit;
-    } else {
-        echo 'error';
+        $file = @ fopen($path, 'rb');
+
+        if ($file) {
+            fpassthru($file);
+            exit;
+        } else {
+            echo 'error';
+        }
     }
 }
 ?>
