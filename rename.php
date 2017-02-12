72ae63ca5e8bb8964344b83ffa33a26097de6990
cassyleigh <cs224@hdm-stuttgart.de>
2017-02-12 21:58:59 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	edit.php
    #	pwändern.php

--- rename.php
+++ rename.php
@@ -6,10 +6,9 @@ include'connection.php';
 $fileID = isset($_GET['fileID']) ? $_GET ['fileID'] : die("ERROR: ID konnte nicht gefunden werden");
 
 $oldname = $file['filename'];
+$newname = $_POST['newname'];
 
 if (isset($_POST['newname'])) {
-
-    $newname = $_POST['newname'];
 
     $statement = $db->prepare("UPDATE file SET filename = :newname WHERE fileID = :fileID");
     $result = $statement->execute(array('filename' => $newname, 'fileID' => $fileID));
@@ -18,13 +17,10 @@ if (isset($_POST['newname'])) {
 
     header("location: upload.php");
 }
-else {
-    echo "Datei konnte nicht umbenannt werden";
-}
 ?>
 
 <form name="rename" action="<?php echo("?id={$fileID}")?>method="post">Neuer Name:<br>
-<input type="text" size="40" maxlength="250" name="newname" value="<?php echo $newname ?>"><br>
+<input type="text" size="40" maxlength="250" name="newname" value= "newname"><br>
 
 <input type="submit" value="Umbenennen">
 </form>
