6b64cc018485a8e842e104e1ce10832693bce306
db104 <db104@hdm-stuttgart.de>
2017-02-12 17:02:26 +0100

    Heil PHP

--- download.php
+++ download.php
@@ -7,7 +7,7 @@ $fileID = $_GET['file'];
 
 $sql = "SELECT filename FROM file WHERE fileID = :fileID";
 $statement = $db->prepare($sql);
-$statement->execute(array('filename'=> $filename));
+$statement->execute(array('fileID'=> $fileID));
 
 $file = $statement->fetch();
 
@@ -27,19 +27,17 @@ if(!$filename) {
 
 $path="Uploads/".$filename;
 $mime=mime_content_type($path);
-$datasize=filesize ($path);
+$fsize=filesize ($path);
 
 
 
     if (file_exists($path) && is_readable($path)) {
-        echo $mime;
-        echo $datasize;
         header('Content-Type: '.$mime);
-        header('Content-Length: '.$datasize);
+        header('Content-Length: '.$fsize);
         header('Content-Disposition: attachment; filename='.$filename);
         header('Content-Transfer-Encoding: binary');
         readfile($path);
-
+       
         
         $file=@fopen($path,'rb');
 
