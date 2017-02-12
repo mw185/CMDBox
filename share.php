d8678741cf695abe94eadc7101060b20a1bb5cf7
db104 <db104@hdm-stuttgart.de>
2017-02-12 18:34:47 +0100

    Heil PHP

--- share.php
+++ share.php
@@ -7,7 +7,19 @@ $sql = $db->prepare('SELECT email FROM person WHERE username = :username');
 $array = array(
     ':username' => $_SESSION['userid']
 );
+
+
+
 $sql->execute($array);
+
+
+
+$sql = "SELECT * FROM file WHERE fileID = :fileID";
+$statement = $db->prepare($sql);
+$statement->execute(array('fileID'=> $fileID));
+
+
+
 $emailto = $_POST["empfaenger"];
 $subject = $_POST["betreff"];
 $body = $_POST["nachricht"];
@@ -15,3 +27,9 @@ $sender = "email";
 $headers = "From:.$sender";
 mail($emailto, $subject, $body, $headers);
 ?>
+
+<form action="share.php" method="POST">
+    <input type="email" name="email">email<br/>
+    <input type="file" name="file">Datei<br/>
+    <input type="submit" value="Senden">Senden<br/>
+</form>
