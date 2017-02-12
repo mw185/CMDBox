440faa2e1b2c83d3d7783eddb1041b604d30e8ef
db104 <db104@hdm-stuttgart.de>
2017-02-12 23:29:46 +0100

    Heil PHP

--- rename.php
+++ rename.php
@@ -5,37 +5,24 @@ include'connection.php';
 
 
 
-$fileID = $_GET['file'];
+$fileID = isset($_GET['fileID']) ? $_GET ['fileID'] : die("ERROR: ID konnte nicht gefunden werden");
 
-$sql = "SELECT * FROM file WHERE fileID = :fileID";
-$statement = $db->prepare($sql);
-$statement->execute(array('fileID'=> $fileID));
+$oldname = $file['filename'];
+$newname = $_POST['newname'];
 
-$file = $statement->fetch();
+if (isset($_POST['newname'])) {
 
-if (isset($file['filename']) && basename($file['filename']) == $file['filename']) {
-    $filename = $file['filename'];
+    $statement = $db->prepare("UPDATE file SET filename = :newname WHERE fileID = :fileID");
+    $result = $statement->execute(array('filename' => $newname, 'fileID' => $fileID));
 
+    rename("Uploads/" . $newname);
 
-//$fileID = isset($_GET['fileID']) ? $_GET ['fileID'] : die("ERROR: ID konnte nicht gefunden werden");
-
-    $oldname = $file['filename'];
-    $newname = $_POST['newname'];
-
-    if (isset($_POST['newname'])) {
-
-        $statement = $db->prepare("UPDATE file SET filename = :newname WHERE fileID = :fileID");
-        $result = $statement->execute(array('filename' => $newname, 'fileID' => $fileID));
-
-        rename("Uploads/" . $newname);
-
-        header("location: upload.php");
-    }
+    header("location: upload.php");
 }
 ?>
 
-<form name="rename" action="<?php echo("?id={$fileID}")?>" method="post"> Neuer Name:<br>
-<input type="text" size="40" maxlength="250" name="newname" value= "newname" placeholder=filename><br>
+<form name="rename" action="<?php echo("?id={$fileID}")?>" method="post">Neuer Name:<br>
+<input type="text" size="40" maxlength="250" name="newname" value= "newname"><br>
 
 <input type="submit" value="Umbenennen">
 </form>
