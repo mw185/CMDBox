358db2c18b6254447476b481b97d403b47f9155b
db104 <db104@hdm-stuttgart.de>
2017-02-12 21:29:05 +0100

    Heil PHP

--- download.php
+++ download.php
@@ -28,13 +28,13 @@ else {
 
 $path = "Uploads/" . $filename;
 $mime = mime_content_type($path);
-$fsize = filesize ($path);
+//$fsize = filesize ($path);
 
 
 
     if (file_exists($path) && is_readable($path)) {
         header('Content-Type: ' . $mime);
-        header('Content-Length: ' . $fsize);
+        //header('Content-Length: ' . $fsize);
         header('Content-Disposition: attachment; filename=' . $filename);
         header('Content-Transfer-Encoding: binary');
         readfile($path);
