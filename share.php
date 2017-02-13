2cda5b1251597548f570b4059f203ae92af5a03a
cassyleigh <cs224@hdm-stuttgart.de>
2017-02-13 22:37:29 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	edit.php
    #	pwändern.php

--- share.php
+++ share.php
@@ -34,6 +34,46 @@ mail($empfaenger, $betreff, $text, "From: $absendername <$absendermail>");
     <input type="submit" value="Senden">Senden<br/>
 </form>
 
+<?php
+include("userdata.inc.php");
+$datei=$_GET['share']; #variable datei wird definiert
+$fulldir=$uploaddir.'/'.$datei; #fulldir wird definiert, uploaddir: $server.$userfolder,
+$sharedir=$home.'/share'; #bestimmt Ordner wo share datei reingelegt wird: share
+$date = new DateTime();
+$folder =  $date->getTimestamp(); #individueller Zeistempel wird erstellt, um datei eindeutigen link zuweisen zu können
+mkdir($home.'share/'.$folder, 0777, true); #mkdir erstellt Verzeichnis (ordner für jede datei, die man teilen will)
+
+copy($fulldir, $home.'share/'.$folder.'/'.$datei);
+$link = $fullurlslash.'share/'.$folder.'/'.$datei; #link Variable wird definiert, die in share.php angezeigt wird
+$_SESSION['link'] = $link;
+header('Location: '.$fullurl.'/share.php'); #weiterleitung zu share.php wo link angezeigt wird
+?>
+
+<?php
+include("inc/header.inc.php");
+$link=$_SESSION['link'];
+?>
+<section id="inhalt">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2><br>Teilen</h2>
+                <br> <br>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <div class="form-group col-xs-12">
+                    <div class="form-group col-xs-12 floating-label-form-group controls">
+                        <input type="text" name="link" size="100" value="<?php echo $link; ?>" class="form-control" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
 
 
 
