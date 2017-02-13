47c2ff2b966bba53a7abff12afe6acc3e7475751
marliswiedemann <mw185@hdm-stuttgart.de>
2017-02-13 17:21:13 +0100

    profi

--- upload.php
+++ upload.php
@@ -35,10 +35,8 @@
 <div>
     <div class="nav">
         <div class="container">
-            <ul class="pull-left">
-                <li><a href="upload.php">CMD Upload</a></li>
-            </ul>
             <ul class="pull-right">
+                <li><a href="upload.php">CMD Upload</a></li>
                 <li><a href="pwaendern.php">Passwort ändern</a></li>
                 <li><a href="profilbild.php">Profilbild ändern</a></li>
                 <li><a href="logout.php">Logout</a></li>
