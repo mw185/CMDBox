e693aeee5f964968362220420d8bc88cb4c87d98
cassyleigh <cs224@hdm-stuttgart.de>
2017-02-13 14:11:45 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	edit.php
    #	pwändern.php

---
+++ download.php
@@ -0,0 +1,43 @@
+<?php
+
+session_start();
+include ("connection.php");
+
+$fileID = $_GET['file'];
+
+$sql = "SELECT * FROM file WHERE fileID = :fileID";
+$statement = $db->prepare($sql);
+$statement->execute(array('fileID'=> $fileID));
+
+$file = $statement->fetch();
+
+if (isset($file['filename']) && basename($file['filename']) == $file['filename']) {
+    $filename = $file['filename'];
+
+
+
+$path = "Uploads/" . $filename;
+$mime = mime_content_type($path);
+$fsize = filesize ($path);
+
+
+
+    if (file_exists($path) && is_readable($path)) {
+        header('Content-Type: ' . $mime);
+        header('Content-Length: ' . $fsize);
+        header("Content-Disposition: attachment; fileID= . '$fileID'");
+        header('Content-Transfer-Encoding: binary');
+        readfile($path);
+        echo $fsize;
+        
+        $file = @ fopen($path, 'rb');
+
+        if ($file) {
+            fpassthru($file);
+            exit;
+        } else {
+            echo 'error';
+        }
+    }
+}
+?>
