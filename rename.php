8b8f2f77d45d34f292b303881afd7df234f811e2
marliswiedemann <mw185@hdm-stuttgart.de>
2017-02-13 13:21:32 +0100

    connection

--- rename.php
+++ rename.php
@@ -1,7 +1,54 @@
-<?php
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width" initial-scale=1.0 />
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+    <script src="./js/dropzone.js"></script>
+    <link href="./css/basic.css" rel="stylesheet">
+    <link href="./css/dropzone.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css">
+    <link href="js/bootstrap.min.js">
+    <link href="profil.css" rel="stylesheet">
+    <link href="pwaendern.css" rel="stylesheet">
+    <title>Passwort ändern</title>
+</head>
 
+<?php
 session_start();
-include'connection.php';
+
+include 'connection.php';
+
+if(isset($errorMessage)) {
+    echo $errorMessage;
+}
+?>
+
+<body>
+
+<ul><li><img src="<?php echo 'Profilbild/'.$_SESSION ['userid'].'.jpg'; ?>" width="285px" alt="Profilbild"/>
+        <h1><?php echo ($_SESSION['userid']) ?></h1></li></ul>
+
+
+
+<div>
+    <div class="nav">
+        <div class="container">
+            <ul class="pull-left">
+                <li><a href="upload.php">CMD Upload</a></li>
+            </ul>
+            <ul class="pull-right">
+                <li><a href="pwaendern.php">Passwort ändern</a></li>
+                <li><a href="profilbild.php">Profilbild ändern</a></li>
+                <li><a href="logout.php">Logout</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+
+<?php
 
 
 $fileID = isset($_GET['fileID'])? $_GET ['fileID']: die("ERROR: ID konnte nicht gefunden werden");
@@ -20,9 +67,15 @@ if (isset($_POST['newname'])) {
 }
 ?>
 
-<form name="rename" action="<?php echo("?fileID={$fileID}")?>" method="post">Neuer Name:<br>
 
-    <input type="text" size="40" maxlength="250" name="newname" value= "Neuer Name" placeholder = "Neuer Name"><br>
 
-    <input type="submit" value="Umbenennen">
+<form name = "rename" enctype="multipart/form-data" action="<?php echo("?fileID={$fileID}")?>" method="post">
+
+    <input type = "text" name="newname" size="60" maxlength="255" id="newname" placeholder="Neuer Name" value="Neuer Name"><br/>
+
+    <input type="Submit" name="submit"  id="submit" value = "Umbenennen">
+
 </form>
+
+</body>
+
