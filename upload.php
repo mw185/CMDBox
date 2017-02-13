3ec30ee847d29fcb195910e54069d4106637688e
db104 <db104@hdm-stuttgart.de>
2017-02-13 23:04:28 +0100

    Genuss Zwieback mjam mjam

--- upload.php
+++ upload.php
@@ -118,7 +118,7 @@ echo "<h2>Bisher hochgeladene Dateien:</h2>";
 
         //while (($entry = readdir($handle)) !== false) {
           // if ($entry != '.' && $entry != '..') {
-echo "<table rules='rows' style='border-color:black'>";
+echo "<table rules='rows' style='width=25px; border-color:black'>";
                 while ($row = $statement->fetch()) {
                     extract($row);
                     echo "<tr>";
