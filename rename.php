76deb63cb83937c0fd68dd7cbe0962512b42ec10
marliswiedemann <mw185@hdm-stuttgart.de>
2017-02-13 12:57:35 +0100

    connection

--- rename.php
+++ rename.php
@@ -4,8 +4,8 @@ session_start();
 include'connection.php';
 
 
+$fileID = isset($_GET['fileID'])? $_GET ['fileID']: die("ERROR: ID konnte nicht gefunden werden");
 
-$fileID = isset($_GET['fileID']) ? $_GET ['fileID'] : die("ERROR: ID konnte nicht gefunden werden");
 
 $oldname = $file['filename'];
 $newname = $_POST['newname'];
@@ -15,14 +15,15 @@ if (isset($_POST['newname'])) {
     $statement = $db->prepare("UPDATE file SET filename = :newname WHERE fileID = :fileID");
     $result = $statement->execute(array('filename' => $newname, 'fileID' => $fileID));
 
-    rename("Uploads/".$newname);
+    rename ("Uploads/". $oldname, $newname);
 
     header("location: upload.php");
 }
 ?>
 
-<form name="rename" action="<?php echo('?fileID={$fileID}')?>" method="post">Neuer Name:<br>
-<input type="text" size="40" maxlength="250" name="newname" value= "newname"><br>
+<form name="rename" action="<?php echo("?fileID={$fileID}")?>" method="post">Neuer Name:<br>
 
-<input type="submit" value="Umbenennen">
+    <input type="text" size="40" maxlength="250" name="newname" value= "Neuer Name" placeholder = "Neuer Name"><br>
+
+    <input type="submit" value="Umbenennen">
 </form>
