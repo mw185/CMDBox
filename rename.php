59f2584a355d6cd9d2e1e41151a087ac949578e5
marliswiedemann <mw185@hdm-stuttgart.de>
2017-02-13 11:58:51 +0100

    connection

--- rename.php
+++ rename.php
@@ -15,13 +15,13 @@ if (isset($_POST['newname'])) {
     $statement = $db->prepare("UPDATE file SET filename = :newname WHERE fileID = :fileID");
     $result = $statement->execute(array('filename' => $newname, 'fileID' => $fileID));
 
-    rename("Uploads/" . $newname);
+    rename("Uploads/".$newname);
 
     header("location: upload.php");
 }
 ?>
 
-<form name="rename" action="<?php echo("?id={$fileID}")?>" method="post">Neuer Name:<br>
+<form name="rename" action="<?php echo('?fileID={$fileID}')?>" method="post">Neuer Name:<br>
 <input type="text" size="40" maxlength="250" name="newname" value= "newname"><br>
 
 <input type="submit" value="Umbenennen">
