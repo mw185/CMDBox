1ef41d6edeb3a6fc3b626570e516c8a9bd058893
marliswiedemann <mw185@hdm-stuttgart.de>
2017-02-14 21:49:10 +0100

    schön

--- profil.php
+++ profil.php
@@ -3,15 +3,17 @@
 // Session starten
 session_start();
 
-if (!isset($_SESSION ['userid'])) {
-    //header("location: login.php");
-    //exit;
+//wenn
+if (!isset($_SESSION ['userid'])) { //es wird geprüft ob eingelogt, ansonsten wird auf login.php weitergeleitet
+    header("location: login.php");
 }
 ?>
 
+<!-- html beginnt-->
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Viewport, Bootstrap und Style wird inkludiert-->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width" initial-scale=1.0 />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
@@ -20,12 +22,15 @@ if (!isset($_SESSION ['userid'])) {
 </head>
 
 <body>
-
+<!-- Logo wird mit div container eingebunden -->
 <div class="extrainfo">
     <img src="CMDBox.png" width="250px" alt="Logo"/>
 </div>
+
+<!-- Profilbild des Users wir aus Datenbank über UserID geholt und dargestellt  -->
 <ul><li><img src="<?php echo 'Profilbild/'.$_SESSION ['userid'].'.jpg'; ?>" width="300px" alt="Profilbild"/></li></ul>
 
+<!-- Menüleiste wird mit div containern als Liste erzeugt; Verlinkt mit entsprechenden Seiten -->
 <div>
     <div class="nav">
         <div class="container">
@@ -34,12 +39,10 @@ if (!isset($_SESSION ['userid'])) {
                 <li><a href="pwaendern.php">Passwort ändern</a></li>
                 <li><a href="profilbild.php">Profilbild ändern</a></li>
                 <li><a href="logout.php">Logout</a></li>
-
             </ul>
         </div>
     </div>
 </div>
-
 </body>
 </html>
 
