29e7def77b565d81235b5289c39422b37070514c
marliswiedemann <mw185@hdm-stuttgart.de>
2017-02-14 15:45:01 +0100

    share

--- share.php
+++ share.php
@@ -1,6 +1,44 @@
 <?php
 session_start();
 include'connection.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width" initial-scale=1.0 />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link href="profil.css" rel="stylesheet">
+    <title>Profil</title>
+</head>
+
+<body>
+
+<div class="extrainfo">
+    <img src="CMDBox.png" width="250px" alt="Logo"/>
+</div>
+<ul><li><img src="<?php echo 'Profilbild/'.$_SESSION ['userid'].'.jpg'; ?>" width="300px" alt="Profilbild"/>
+        <h1><?php echo ($_SESSION['userid']) ?></h1></li></ul>
+
+<div>
+    <div class="nav">
+        <div class="container">
+            <ul class="pull-right">
+                <li><a href="upload.php">CMD Upload</a></li>
+                <li><a href="pwaendern.php">Passwort ändern</a></li>
+                <li><a href="profilbild.php">Profilbild ändern</a></li>
+                <li><a href="logout.php">Logout</a></li>
+
+            </ul>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
+
+<?php
 
 ///Daten aus DB herauslesen
 $sql = $db->prepare('SELECT username FROM person WHERE username = :username');
@@ -31,6 +69,6 @@ mail($empfaenger, $betreff, $text, "From: $absendername <$absendermail>");
 
 <form name ="share" action="<?php echo("?fileID={$fileID}")?>" method="POST">
     <input type="email" name="email">email<br/>
-    <input type="submit" value="Senden">Senden<br/>
+    <input type="submit" value="Senden"><br/>
 </form>
 
