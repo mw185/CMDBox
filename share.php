15c370dc6e6bfab1e9e13078a70ededea0cb5548
marliswiedemann <mw185@hdm-stuttgart.de>
2017-02-14 15:27:00 +0100

    Merge branch 'master' of /Users/marliswiedemann/PhpstormProjects/CMDBox with conflicts.

--- share.php
+++ share.php
@@ -9,7 +9,7 @@ $array = array(
 );
 
 $fileID = isset($_GET['fileID'])? $_GET ['fileID']: die("ERROR: ID konnte nicht gefunden werden");
-$username = isset($_GET['username']);
+$username = $_SESSION['userid'];
 
 $sql->execute($array);
 
@@ -25,7 +25,7 @@ $absendermail = "automatic@CMD-Box.de";
 $betreff = "$username hat Dir eine Datei gesendet!";
 $text = "Der Nutzer $username hat dir eine Datei gesendet.
 Du kannst die Datei unter folgendem Link herunterladen:
-https://mars.iuk.hdm-stuttgart.de/~mw185/Uploads/fileID=$fileID";
+https://mars.iuk.hdm-stuttgart.de/~mw185/download.php?fileID=$fileID";
 mail($empfaenger, $betreff, $text, "From: $absendername <$absendermail>");
 ?>
 
