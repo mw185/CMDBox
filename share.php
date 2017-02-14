e961d82b5f75f7dca6698601919ca848618e299e
marliswiedemann <mw185@hdm-stuttgart.de>
2017-02-14 19:40:26 +0100

    schön

--- share.php
+++ share.php
@@ -20,8 +20,7 @@ include'connection.php'; ##Datenbankverbindung wird hergestellt, indem connectio
 <div class="extrainfo">
     <img src="CMDBox.png" width="250px" alt="Logo"/>
 </div>
-<ul><li><img src="<?php echo 'Profilbild/'.$_SESSION ['userid'].'.jpg'; ?>" width="300px" alt="Profilbild"/>
-        <h1><?php echo ($_SESSION['userid']) ?></h1></li></ul>
+<ul><li><img src="<?php echo 'Profilbild/'.$_SESSION ['userid'].'.jpg'; ?>" width="300px" alt="Profilbild"/></li></ul>
 
 <div>
     <div class="nav">
